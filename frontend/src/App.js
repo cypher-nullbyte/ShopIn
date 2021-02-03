5e092ecf9c268b3aa758457783e919efb0eea5bb
cYpHeR <github.com/cypher-nullbyte>
2021-02-03 20:08:21 +0530

    Shipped @{Initialization} Template to ReactApp Successfully

---
+++ frontend/src/App.js
@@ -0,0 +1,148 @@
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="index.html">ShopIn</a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
+      </header>
+      <content>
+            <div className="row center">
+                <div className="card">
+                    <div className="card__img-container">
+                        <img className="card__img" src="https://picsum.photos/200/500" alt="" />
+                    </div>
+                    <div className="card__content">
+                        <h2 className="card__title">A cool product</h2>
+                        <p className="card__subtitle">that you should buy</p>
+                        <p className="card__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                        deserunt mollit anim id est laborum.</p>
+                        <p className="card__price">$399.99</p>
+                        <a href="#" className="card__button">Buy now</a>
+                        <div className="rating" style={{"text-align": "right"}}>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                        </div>
+                    </div>
+                </div> 
+                <div className="card">
+                    <div className="card__img-container">
+                        <img className="card__img" src="https://picsum.photos/200/500" alt="" />
+                    </div>
+                    <div className="card__content">
+                        <h2 className="card__title">A cool product</h2>
+                        <p className="card__subtitle">that you should buy</p>
+                        <p className="card__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                        deserunt mollit anim id est laborum.</p>
+                        <p className="card__price">$399.99</p>
+                        <a href="#" className="card__button">Buy now</a>
+                        <div className="rating" style={{"text-align": "right"}}>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                        </div>
+                    </div>
+                </div> 
+                <div className="card">
+                    <div className="card__img-container">
+                        <img className="card__img" src="https://picsum.photos/200/500" alt="" />
+                    </div>
+                    <div className="card__content">
+                        <h2 className="card__title">A cool product</h2>
+                        <p className="card__subtitle">that you should buy</p>
+                        <p className="card__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                        deserunt mollit anim id est laborum.</p>
+                        <p className="card__price">$399.99</p>
+                        <a href="#" className="card__button">Buy now</a>
+                        <div className="rating" style={{"text-align": "right"}}>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                        </div>
+                    </div>
+                </div> 
+                <div className="card">
+                    <div className="card__img-container">
+                        <img className="card__img" src="https://picsum.photos/200/500" alt="" />
+                    </div>
+                    <div className="card__content">
+                        <h2 className="card__title">A cool product</h2>
+                        <p className="card__subtitle">that you should buy</p>
+                        <p className="card__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                        deserunt mollit anim id est laborum.</p>
+                        <p className="card__price">$399.99</p>
+                        <a href="#" className="card__button">Buy now</a>
+                        <div className="rating" style={{"text-align": "right"}}>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                        </div>
+                    </div>
+                </div> 
+                <div className="card">
+                    <div className="card__img-container">
+                        <img className="card__img" src="https://picsum.photos/200/500" alt="" />
+                    </div>
+                    <div className="card__content">
+                        <h2 className="card__title">A cool product</h2>
+                        <p className="card__subtitle">that you should buy</p>
+                        <p className="card__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                        deserunt mollit anim id est laborum.</p>
+                        <p className="card__price">$399.99</p>
+                        <a href="#" className="card__button">Buy now</a>
+                        <div className="rating" style={{"text-align": "right"}}>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                        </div>
+                    </div>
+                </div> 
+                <div className="card">
+                    <div className="card__img-container">
+                        <img className="card__img" src="https://picsum.photos/200/500" alt="" />
+                    </div>
+                    <div className="card__content">
+                        <h2 className="card__title">A cool product</h2>
+                        <p className="card__subtitle">that you should buy</p>
+                        <p className="card__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                        deserunt mollit anim id est laborum.</p>
+                        <p className="card__price">$399.99</p>
+                        <a href="#" className="card__button">Buy now</a>
+                        <div className="rating" style={{"text-align": "right"}}>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                        </div>
+                    </div>
+                </div>          
+            </div>
+        </content>
+        <footer className="row center">&copy;ShopIn.Inc 2020</footer>
+    </div>
+  );
+}
+
+export default App;
