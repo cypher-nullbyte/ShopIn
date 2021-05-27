fd168e1fce0b61491d3651f4965757ef78641624
cYpHeR <64411912+cypher-nullbyte@users.noreply.github.com>
2021-05-28 03:59:27 +0530

    Add Events to admin Chat Screen

--- frontend/src/screens/SupportScreen.js
+++ frontend/src/screens/SupportScreen.js
@@ -1,8 +1,86 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
 import MessageBox from '../components/MessageBox';
+import socketIOClient from 'socket.io-client';
+
+let allUsers=[];
+let allMessages=[];
+let allSelecteduser=[];
+const ENDPOINT= window.location.host.indexOf('localhost') >=0
+    ? 'http://127.0.0.1:5000'
+    : window.location.host;
+
 
 export default function SupportScreen(){
-    
+    const [selectedUser,setSelectedUser]=useState({});
+    const [socket,setSocket]=useState(null);
+    const uiMessagesRef=useRef(null);
+    const [messageBody,setMessageBody]=useState('');
+    const [messages,setMessages]=useState([]);
+    const [users,setUsers]=useState([]);
+    const userSignin=useSelector(state=>state.userSignin);
+    const {userInfo}=userSignin;
+
+    useEffect(()=>{
+        if(uiMessagesRef.current){
+            uiMessagesRef.current.scrollBy({
+                top:uiMessagesRef.current.clientHeight,
+                left:0,
+                behaviour:'smooth',
+            });
+            let sk;
+            if(!socket){
+                sk=socketIOClient(ENDPOINT);
+                setSocket(sk);
+            }
+            sk.emit('onLogin',{
+                _id:userInfo._id,
+                name:userInfo.name,
+                isAdmin:userInfo.isAdmin,
+            });
+            sk.on('message',data=>{
+                if(allSelecteduser._id===data._id){
+                    allMessages=[...allMessages,data];
+                }
+                else{
+                    const existUser=allUsers.find(user=>user._id===data._id);
+                    if(existUser){
+                        allUsers=allUsers.map(user=>(
+                            user._id===existUser._id ? {...user, unread:true} : user
+                        ));
+                        setUsers(allUsers);
+                    }
+                }
+                setMessages(allMessages);
+            });
+            sk.on('updateUser',updatedUser=>{
+                const existUser=allUsers.find(user=>user._id===updatedUser._id);
+                if(existUser){
+                    allUsers=allUsers.map(user=>
+                        user._id===existUser._id ? updatedUser : user    
+                    );
+                    setUsers(allUsers);
+                }
+                else{
+                    allUsers=[...allUsers,updatedUser];
+                    setUsers(allUsers);
+                }
+            });
+
+            sk.on('listUsers',updatedUser=>{
+                allUsers=updatedUser;
+                setUsers(allUsers);
+            });
+
+            sk.on('selectedUser',user=>{
+                allMessages=user.messages;
+                setMessages(allMessages);
+            })
+
+        }
+        
+    },[messages,socket,users]);
+
     return(
         <div className="row top full-container">
             <div className="col-1 support-users">
